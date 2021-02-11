daf54ff288cbd9c0f01765b9b975e264c4d833c6
BlueLondon1 <46319471+BlueLondon1@users.noreply.github.com>
2021-02-11 14:27:21 +0100

    DeletingSomeExercice

--- keyboard.py
+++
@@ -1,8 +0,0 @@
-from pynput.keyboard import Key, Controller
-import time
-
-keyboard = Controller() #Create the controller
-
-time.sleep(2)
-keyboard.press('Coucou tu veux voir ma bite')
-keyboard.release('Coucou tu veux voir ma bite')
